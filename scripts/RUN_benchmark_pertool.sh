43f809e75551f37f34aebd055c9da9be1b07ab91
Michael Apostolides <michael.j.apostolides@gmail.com>
2020-08-18 15:39:01 -0400

    benchmark pertool scripts

---
+++ scripts/RUN_benchmark_pertool.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+#Change date to current date
+date=Aug-18-2020
+
+#DATASETS
+sim45_sim52=1
+#brca_4=0
+#beers_neg=1
+#sim_50=1
+#sim101=1
+
+
+#REFERENCE FILES 
+gene_bed=/MetaFusion/reference_files/ens_known_genes.renamed.ENSG.bed
+gene_info=/MetaFusion/reference_files/Homo_sapiens.gene_info
+genome_fasta=/MetaFusion/reference_files/human_g1k_v37_decoy.fasta
+recurrent_bedpe=/MetaFusion/reference_files/blacklist_breakpoints.bedpe
+fusiontools=/MetaFusion/scripts
+runs_dir=/MetaFusion/RUNS
+
+
+outdir=$1
+mkdir -p $outdir
+truth_fusions=$2
+cff=$3
+cluster=$4
+fusiontools=$5
+
+# SIM45.SIM52.combined
+if [ $sim45_sim52 -eq 1 ]; then
+echo SIM45.SIM52
+outdir=$runs_dir/SIM45.SIM52.pertool_benchmark.$date
+echo generating output in $outdir
+mkdir $outdir
+cff=/MetaFusion/test_data/cff/dream.sim45.sim52.cff
+truth_fusions=/MetaFusion/test_data/truth_sets/dream.sim45.sim52.truth_set.dat
+
+$fusiontools/benchmarking_cff_pertool.sh $outdir $truth_fusions $cff $fusiontools
+
+fi
+
+#BT474.KPL4.MCF7.SKBR3
+if [ $brca_4 -eq 1 ]; then
+echo BT474.KPL4.MCF7.SKBR3
+outdir=$runs_dir/BT474.KPL4.MCF7.SKBR3.pertool_benchmark.$date
+echo generating output in $outdir
+truth_fusions=/MetaFusion/test_data/truth_sets/BRCA.truth_set.dat
+cff=/MetaFusion/test_data/cff/BRCA.cff
+
+$fusiontools/benchmarking_cff_pertool.sh $outdir $truth_fusions $cff $fusiontools
+fi
+
+#NEGATIVE CONTROL BEERS
+if [ $beers_neg -eq 1 ]; then
+echo NEGATIVE CONTROL BEERS
+outdir=$runs_dir/NEG_CONTROL_BEERS.pertool_benchmark.$date
+echo generating output in $outdir
+cff=/MetaFusion/test_data/cff/beers_neg.cff 
+truth_fusions=/MetaFusion/test_data/truth_sets/BRCA.truth_set.dat
+
+$fusiontools/benchmarking_cff_pertool.sh $outdir $truth_fusions $cff $fusiontools
+fi
+
+# SIM50 2500 fusions files:
+if [ $sim_50 -eq 1 ]; then
+echo SIM50
+outdir=$runs_dir/SIM50.pertool_benchmark.$date
+echo generating output in $outdir
+cff=/MetaFusion/test_data/cff/sim50.cff
+truth_fusions=/MetaFusion/test_data/truth_sets/sim50.truth_set.dat
+
+$fusiontools/benchmarking_cff_pertool.sh $outdir $truth_fusions $cff $fusiontools
+fi
+
+#SIM101 2500 fusions files, same truth set as SIM50
+if [ $sim101 -eq 1 ]; then
+echo SIM101
+outdir=$runs_dir/SIM101.pertool_benchmark.$date
+echo generating output in $outdir
+cff=/MetaFusion/test_data/cff/sim101.cff
+truth_fusions=/MetaFusion/test_data/truth_sets/sim101.truth_set.dat
+
+$fusiontools/benchmarking_cff_pertool.sh $outdir $truth_fusions $cff $fusiontools
+fi
---
+++ scripts/benchmarking_cff_pertool.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#INPUTS
+outdir=$1
+mkdir -p $outdir
+truth_fusions=$2
+cff=$3
+fusiontools=$4
+FUSION_BENCHMARK=$fusiontools/FusionBenchmarking
+
+#CALLERS
+callers=$(echo arriba star_fusion star_seqr defuse ericscript integrate fusionmap chimerascan)
+
+# Add FID to unannotated .cff
+python $fusiontools/add_FID_to_cff.py $cff > $outdir/$(basename $cff).FID
+cff=$outdir/$(basename $cff).FID
+
+#PUT CFF INTO PROPER FORMAT
+outfile=$outdir/cff.preds.collected
+echo -e "sample\tprog\tfusion\tJ\tS\tFID" > $outfile
+cat $cff | awk '{FS=OFS="\t"}{print $8,$11,$14"--"$16, $12, $13, $31}' >> $outfile
+
+echo Mapping gene partners to Gencode v19 genes 
+$perl_bin ${FUSION_BENCHMARK}/benchmarking/map_gene_symbols_to_gencode_FID.pl \
+   $outfile \
+   ${FUSION_BENCHMARK}/resources/genes.coords.HGNC_renamed_added.gz \
+   ${FUSION_BENCHMARK}/resources/genes.aliases \
+   > $outdir/$(basename $outfile).gencode_mapped
+outfile=$outdir/$(basename $outfile).gencode_mapped
+
+echo Scoring of fusion predictions
+${FUSION_BENCHMARK}/benchmarking/fusion_preds_to_TP_FP_FN_FID.pl --truth_fusions $truth_fusions --fusion_preds $outfile  --allow_reverse_fusion > $outdir/$(basename $outfile).scored 
+outfile=$outdir/$(basename $outfile).scored
+
+
+#GET TP/FP/FN
+echo -e caller\\tTP\\tFP\\tFN > $outdir/$(basename $outfile).TP_FP_counts
+for caller in ${callers[@]};do 
+  # per-caller cff and outfile
+  cat $cff | grep $caller > $cff.$caller 
+  outfile_caller=$outdir/$(basename $outfile).$caller
+  cat $outfile | grep $caller > $outfile_caller
+
+  # TP/FP/FN outfiles
+  cat $outfile_caller | awk '$1=="TP" || $1=="NA-TP"' > $outfile_caller.TP
+  cat $outfile_caller | awk '$1=="FP" || $1=="NA-FP"' > $outfile_caller.FP
+  cat $outfile_caller | awk '$1=="FN"' > $outfile_caller.FN
+  # TP/FP/FN counts
+  TP=$(cat $outfile_caller | awk '$1=="TP"'  | wc | awk '{print $1}') 
+  FP=$(cat $outfile_caller.FP  | awk '$1=="FP"' | wc | awk '{print $1}')
+  FN=$(cat $outfile_caller.FN | awk '$1=="FN"'| wc | awk '{print $1}')
+  echo -e $caller\\t$TP\\t$FP\\t$FN >> $outdir/$(basename $outfile).TP_FP_counts
+
+  #generate per-caller .cff for TP and FP
+  #TP_IDs=$( cat $outfile_caller.TP | awk '{print $NF}' | sort | uniq)
+  #FP_IDs=$( cat $outfile_caller.FP | awk '{print $NF}' | sort | uniq)
+  ##TP
+  #rm $cff.$caller.TP
+  #for id in ${TP_IDs[@]}; do
+  #  cat $cff.$caller | grep $id >> $cff.$caller.TP;
+  #done
+  ##FP
+  #rm $cff.$caller.FP
+  #for id in ${FP_IDs[@]}; do
+  #  cat $cff.$caller | grep $id >> $cff.$caller.FP;
+  #done
+done
+
