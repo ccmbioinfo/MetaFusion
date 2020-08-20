697764ea1f76b4fdbb0631494d50fdeb4fafdfcc
Michael Apostolides <mapostolides@Michaels-MacBook-Pro.local>
2020-08-20 13:39:21 -0400

    changed black --> block

--- scripts/blocklist_filter_recurrent_breakpoints.sh
+++ scripts/blocklist_filter_recurrent_breakpoints.sh
@@ -5,18 +5,18 @@ cluster=$2
 outdir=$3
 recurrent_bedpe=$4
 
-#BLACKLIST FILTER
+#BLOCKLIST FILTER
 cff_bedpe=$(basename $cff).bedpe
 cat $cff | awk '{FS = OFS = "\t"}{print $1,$2,$2+1,$4,$5,$5+1,$31}' | sed 's/chr//g' > $outdir/$cff_bedpe 
 pairToPair -a $recurrent_bedpe -b $outdir/$cff_bedpe -slop 5 > $outdir/recurrent_filtered_fusions.bedpe
 
-# Make sorted blacklist file
+# Make sorted blocklist file
 ids=$(cat $outdir/recurrent_filtered_fusions.bedpe | awk '{print $NF}' | sort | uniq)
 cat $outdir/recurrent_filtered_fusions.bedpe | awk '{print $NF}' | sort | uniq > $outdir/FIDS
-for FID in ${ids[@]};do cat $cluster | grep $FID ;done | sort | uniq  >  $outdir/$(basename $cluster).BLACKLIST
+for FID in ${ids[@]};do cat $cluster | grep $FID ;done | sort | uniq  >  $outdir/$(basename $cluster).BLOCKLIST
 
 # Sort cluster, required for "comm" command
 cat $cluster | sort > $outdir/$(basename $cluster).sorted
 
-# Remove lines of BLACKLIST file from cluster, creating .blck_filter file
-comm -2 -3 $outdir/$(basename $cluster).sorted $outdir/$(basename $cluster).BLACKLIST 
+# Remove lines of BLOCKLIST file from cluster, creating .blck_filter file
+comm -2 -3 $outdir/$(basename $cluster).sorted $outdir/$(basename $cluster).BLOCKLIST 
