f0a9792616bd3939657e8155216cf35aa5a71c23
pintoa1-mskcc <pintoa1@mskcc.org>
2023-06-05 13:13:21 -0400

    make this runnable all the way through in new docker

--- scripts/RUN_cluster_exons.sh
+++ scripts/RUN_cluster_exons.sh
@@ -7,13 +7,13 @@ fusiontools_dir=$3
 
 #Generate intersections file for exons 
 fid_intersection_file=$outdir/FID.intersections.exons.tsv
-python $fusiontools_dir/intersect_exons.py $cff > $fid_intersection_file 
+python intersect_exons.py $cff > $fid_intersection_file
 
 # Graph clustering
 fid_clusters_file=$outdir/FID.clusters.tsv
 #ls -l $fid_intersection_file
 #ls -l $fusiontools_dir/cluster_intersections.local.R
-Rscript $fusiontools_dir/cluster_intersections.R $fid_intersection_file $fid_clusters_file 
+Rscript cluster_intersections.R $fid_intersection_file $fid_clusters_file
 
 # generate cluster file using clustered FIDs and cff file
-python $fusiontools_dir/generate_cluster_file.py $cff $fid_clusters_file
+python generate_cluster_file.py $cff $fid_clusters_file
--- scripts/RUN_cluster_genes_breakpoints.sh
+++ scripts/RUN_cluster_genes_breakpoints.sh
@@ -3,17 +3,17 @@
 #module load python/2.7.11
 cff=$1
 outdir=$2
-fusiontools_dir=$3
+
 
 #Generate intersections file for both gene name and breakpoint
 fid_intersection_file=$outdir/FID.intersections.tsv
-python $fusiontools_dir/intersect_breakpoints_and_gene_names.py $cff > $fid_intersection_file 
+python intersect_breakpoints_and_gene_names.py $cff > $fid_intersection_file
 
 # Graph clustering
 fid_clusters_file=$outdir/FID.clusters.tsv
 #ls -l $fid_intersection_file
 #ls -l $fusiontools_dir/cluster_intersections.local.R
-Rscript $fusiontools_dir/cluster_intersections.R $fid_intersection_file $fid_clusters_file 
+Rscript cluster_intersections.R $fid_intersection_file $fid_clusters_file
 
 # generate cluster file using clustered FIDs and cff file
-python $fusiontools_dir/generate_cluster_file.py $cff $fid_clusters_file
+python generate_cluster_file.py $cff $fid_clusters_file
